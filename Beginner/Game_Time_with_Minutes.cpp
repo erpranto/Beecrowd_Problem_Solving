bef6365069b351bee06b9f12e7ca2aeed63834ec
erpranto <erpranto55@gmail.com>
2025-09-19 19:07:44 +0600

    feat: add solution for game duration calculation problem

---
+++ Beginner/Game_Time_with_Minutes.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h1, m1, h2, m2;
+    cin >> h1 >> m1 >> h2 >> m2;
+
+    int start = h1 * 60 + m1;
+    int end = h2 * 60 + m2;
+
+    if (end <= start) {
+        end += 24 * 60;
+    }
+
+    int duration = end - start;
+    int hours = duration / 60;
+    int minutes = duration % 60;
+
+    cout << "O JOGO DUROU " << hours << " HORA(S) E " << minutes << " MINUTO(S)" << endl;
+    return 0;
+}
